795e6dc15c4e5bca6328b75572b2545da3a303f4
josephesteva <joe.esteva@gmail.com>
2023-11-16 14:20:42 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+// Component to get list of pokemon
+	// Fetch list of pokemon from the api
+	// Store pokemon in an array
+	// Map through each item in the array, creating a card for each pokemon
+
+// Component to render a pokemon card	
+	// Render a pokemon card upon state change
+	// Set card so that additional details are displayed if pokemon card is clicked
+
+
+// Create form to add new pokemon to the array
+
+function App() {
+
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
+
